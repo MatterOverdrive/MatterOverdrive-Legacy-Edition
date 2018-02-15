1deb478c6b40d04a5802083ad05e0a1598c9beb1
TheCodedOne <dr.kcodex@gmail.com>
2018-02-15 23:46:21 +0000

    Begin planning out new sounds

---
+++ src/main/java/matteroverdrive/sounds/MOSounds.java
@@ -0,0 +1,40 @@
+package matteroverdrive.sounds;
+
+public final class MOSounds {
+
+    public static final class Story {
+        public enum Misc {
+            INTRO,
+        }
+    }
+
+    public enum ShipMisc {
+
+    }
+    public enum ShipAi {
+        LIFESUPPORT_ACTIVATE,
+        LIFESUPPORT_DEACTIVATE,
+        POWERUNIT_ACTIVATE,
+        POWERUNIT_DEACTIVATE,
+        HYDRAULICS_ACTIVATE,
+        HYDRAULICS_DEACTIVATE,
+        TELEMETRICS_ACTIVATE,
+        EXIT_VECTOR_CALCULATED,
+        TELEMETRICS_DEACTIVATE,
+        COMMS_ACTIVATE,
+        COMMS_INCOMMING,
+        COMMS_OUTGOING,
+        COMMS_DEACTIVATE,
+        COMPUTER_ACTIVATE,
+        COMPUTER_DEACTIVATE,
+        READY_LAUNCH,
+        CONFIRM_COORDS,
+        COUNTDOWN,
+        APPROCH_ORBITAL_DEBRIS,
+        APPROCH_UNKNOWN_OBSTICLE,
+        DEBRIS_CLEAR,
+        PERFORM_GRAVITY_TURN,
+        ENGAGE_BOOST,
+        ENGAGE_WARP,
+    }
+}
