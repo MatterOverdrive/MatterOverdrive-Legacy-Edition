6dba2092dbd29ccff29f8e2e27b70275be4d46b9
DBotThePony <dbotthepony@yandex.ru>
2018-02-16 07:01:18 +0700

    Fixed energy issues (#50)
    * Fixed energy issues
    
    * Fixed infinite charging
    
    * Changed tag names to old ones
    
    * Moved nbt tag under energy/, misc fixes
    
    * Reverted getStorage change and renamed some stuff

--- src/main/java/matteroverdrive/items/includes/EnergyContainer.java
+++ src/main/java/matteroverdrive/items/includes/EnergyContainer.java
@@ -1,10 +1,15 @@
 package matteroverdrive.items.includes;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.energy.EnergyStorage;
 
+import javax.annotation.Nullable;
+
 public class EnergyContainer extends EnergyStorage implements INBTSerializable<NBTTagCompound> {
+    private ItemStack stack;
+
     public EnergyContainer(int capacity) {
         super(capacity);
     }
@@ -25,13 +30,128 @@ public class EnergyContainer extends EnergyStorage implements INBTSerializable<N
         setEnergy(this.capacity);
     }
 
+    @Nullable
+    public ItemStack getItemStack() {
+        return this.stack;
+    }
+
+    public EnergyContainer setItemStack(ItemStack stack) {
+        boolean hasTags = stack.hasTagCompound();
+        if (!hasTags || !stack.getTagCompound().hasKey("energy")) {
+            if (!hasTags) {
+                stack.setTagCompound(new NBTTagCompound());
+            }
+
+            NBTTagCompound tag = stack.getTagCompound();
+            tag.setTag("energy",  serializeNBT());
+        } else {
+            deserializeNBT(stack.getTagCompound().getCompoundTag("energy"));
+        }
+
+        this.stack = stack;
+        return this;
+    }
+
+    private NBTTagCompound getStackEnergyTag() {
+        return stack.getTagCompound().getCompoundTag("energy");
+    }
+
+    @Override
+    public int getEnergyStored() {
+        if (stack != null) {
+            return getStackEnergyTag().getInteger("energy");
+        }
+
+        return energy;
+    }
+
+    @Override
+    public int getMaxEnergyStored() {
+        if (stack != null) {
+            return getStackEnergyTag().getInteger("capacity");
+        }
+
+        return capacity;
+    }
+
+    public int getMaxExtract() {
+        if (stack != null) {
+            return getStackEnergyTag().getInteger("maxExtract");
+        }
+
+        return maxExtract;
+    }
+
+    public int getMaxReceive() {
+        if (stack != null) {
+            return getStackEnergyTag().getInteger("maxReceive");
+        }
+
+        return maxReceive;
+    }
+
     public void setEnergy(int energy) {
+        if (stack != null) {
+            getStackEnergyTag().setInteger("energy", energy);
+
+            if (getEnergyStored() > getMaxEnergyStored()) {
+                setFull();
+            }
+
+            return;
+        }
+
         this.energy = energy;
         if (this.energy > this.capacity)
             setFull();
     }
 
     @Override
+    public boolean canExtract() {
+        return getMaxExtract() > 0;
+    }
+
+    @Override
+    public boolean canReceive() {
+        return getMaxReceive() > 0;
+    }
+
+    @Override
+    public int receiveEnergy(int amount, boolean simulate) {
+        if (!canReceive())
+            return 0;
+
+        int energyReceived = Math.min(getMaxEnergyStored() - getEnergyStored(), Math.min(getMaxReceive(), amount));
+
+        if (!simulate) {
+            if (stack != null && energyReceived != 0) {
+                getStackEnergyTag().setInteger("energy", getEnergyStored() + energyReceived);
+            } else {
+                energy += energyReceived;
+            }
+        }
+
+        return energyReceived;
+    }
+
+    @Override
+    public int extractEnergy(int amount, boolean simulate) {
+        if (!canExtract())
+            return 0;
+
+        int energyExtracted = Math.min(getEnergyStored(), Math.min(getMaxExtract(), amount));
+
+        if (!simulate) {
+            if (stack != null && energyExtracted != 0) {
+                getStackEnergyTag().setInteger("energy", getEnergyStored() - energyExtracted);
+            } else {
+                energy -= energyExtracted;
+            }
+        }
+
+        return energyExtracted;
+    }
+
     public NBTTagCompound serializeNBT() {
         NBTTagCompound tag = new NBTTagCompound();
         tag.setInteger("energy", energy);
@@ -41,7 +161,6 @@ public class EnergyContainer extends EnergyStorage implements INBTSerializable<N
         return tag;
     }
 
-    @Override
     public void deserializeNBT(NBTTagCompound tag) {
         energy = tag.getInteger("energy");
         capacity = tag.getInteger("capacity");
@@ -50,6 +169,11 @@ public class EnergyContainer extends EnergyStorage implements INBTSerializable<N
     }
 
     public void setMaxEnergy(int max) {
+        if (stack != null) {
+            getStackEnergyTag().setInteger("capacity", max);
+            return;
+        }
+
         this.capacity = max;
     }
 }
