c0954845978a8e01316d644a3aadacc29a25b615
TheCodedOne <dr.kcodex@gmail.com>
2018-02-20 14:12:37 +0000

    Fix inventories doing.. pretty retarded shit

--- src/main/java/matteroverdrive/util/MatterHelper.java
+++ src/main/java/matteroverdrive/util/MatterHelper.java
@@ -114,9 +114,7 @@ public class MatterHelper {
         if (item instanceof ItemBlock) {
             Block block = Block.getBlockFromItem(item);
 
-            if (block == Blocks.BEDROCK || block == Blocks.AIR) {
-                return false;
-            }
+            return block != Blocks.BEDROCK && block != Blocks.AIR;
         }
 
         return true;
@@ -139,30 +137,22 @@ public class MatterHelper {
             for (int i1 = 0; i1 < inventory.getSizeInventory(); ++i1) {
                 ItemStack itemstack = inventory.getStackInSlot(i1);
 
-                if (itemstack != null) {
+                if (!itemstack.isEmpty()) {
                     float f = world.rand.nextFloat() * 0.8F + 0.1F;
                     float f1 = world.rand.nextFloat() * 0.8F + 0.1F;
                     float f2 = world.rand.nextFloat() * 0.8F + 0.1F;
 
-                    while (itemstack.getCount() > 0) {
-                        int j1 = world.rand.nextInt(21) + 10;
+                    EntityItem entityitem = new EntityItem(world, (double) ((float) pos.getX() + f), (double) ((float) pos.getY() + f1), (double) ((float) pos.getZ() + f2), itemstack);
 
-                        if (j1 > itemstack.getCount()) {
-                            j1 = itemstack.getCount();
-                        }
-                        itemstack.shrink(j1);
-                        EntityItem entityitem = new EntityItem(world, (double) ((float) pos.getX() + f), (double) ((float) pos.getY() + f1), (double) ((float) pos.getZ() + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
-
-                        if (itemstack.hasTagCompound()) {
-                            entityitem.getItem().setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
-                        }
-
-                        float f3 = 0.05F;
-                        entityitem.motionX = (double) ((float) world.rand.nextGaussian() * f3);
-                        entityitem.motionY = (double) ((float) world.rand.nextGaussian() * f3 + 0.2F);
-                        entityitem.motionZ = (double) ((float) world.rand.nextGaussian() * f3);
-                        world.spawnEntity(entityitem);
+                    if (itemstack.hasTagCompound()) {
+                        entityitem.getItem().setTagCompound(itemstack.getTagCompound().copy());
                     }
+
+                    float f3 = 0.05F;
+                    entityitem.motionX = (double) ((float) world.rand.nextGaussian() * f3);
+                    entityitem.motionY = (double) ((float) world.rand.nextGaussian() * f3 + 0.2F);
+                    entityitem.motionZ = (double) ((float) world.rand.nextGaussian() * f3);
+                    world.spawnEntity(entityitem);
                 }
             }
             return true;
