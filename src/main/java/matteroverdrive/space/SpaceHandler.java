eb00d0de7dadf16774aca4423020bbb63a761354
TheCodedOne <dr.kcodex@gmail.com>
2018-03-02 14:10:22 +0000

    Texture the plaent

--- src/main/java/matteroverdrive/space/SpaceHandler.java
+++ src/main/java/matteroverdrive/space/SpaceHandler.java
@@ -9,16 +9,18 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.Sphere;
-
-import static org.lwjgl.opengl.GL11.*;
 
 public class SpaceHandler {
     private float rotation = 0;
     private Sphere sphere = new Sphere();
+
+    public static ResourceLocation texture = new ResourceLocation(Reference.PATH_GUI + "map.png");
 
     @SubscribeEvent
     public void renderWorldLast(RenderWorldLastEvent event) {
@@ -26,19 +28,22 @@ public class SpaceHandler {
         EntityPlayer player = mc.player;
         if (player.dimension == MatterOverdrive.DIMENSION_HANDLER.SPACE_TYPE.getId()) {
             GlStateManager.pushMatrix();
-            RenderUtils.applyColor(Reference.COLOR_HOLO_RED);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            GlStateManager.disableTexture2D();
+            GlStateManager.color(1, 1, 1, 1);
+            GlStateManager.disableLighting();
             GlStateManager.translate(-TileEntityRendererDispatcher.staticPlayerX, -TileEntityRendererDispatcher.staticPlayerY + -600, -TileEntityRendererDispatcher.staticPlayerZ);
             GlStateManager.rotate(45, 1, 0, 0);
             GlStateManager.rotate(rotation % 360, 0, 0, 1);
-            sphere.draw(600f, 64, 32);
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             GlStateManager.enableTexture2D();
+            mc.getTextureManager().bindTexture(texture);
+            sphere.setTextureFlag(true);
+            sphere.draw(600f, 64, 32);
             GlStateManager.popMatrix();
             rotation += event.getPartialTicks() / 40;
             if (rotation >= 360)
                 rotation -= 360;
+
         }
     }
 
@@ -48,15 +53,15 @@ public class SpaceHandler {
         int dimension = living.dimension;
         double amount = 1;
         if (dimension == MatterOverdrive.DIMENSION_HANDLER.SPACE_TYPE.getId())
-            amount = 0.01;
+            amount = 0.05;
         if (dimension == 1)
             amount = 0.5;
-        if (!living.world.isRemote && amount != 1) {
+        if (amount != 1) {
             if (!(living instanceof EntityPlayer) || !(((EntityPlayer) living).capabilities.isFlying)) {
                 living.motionY += 0.0784000015258789;
                 living.motionY -= 0.0784000015258789 * amount;
             }
-            if (living instanceof EntityPlayer && living.posY <= -3)
+            if (!living.world.isRemote && living instanceof EntityPlayer && living.posY <= -3)
                 CustomTeleporter.teleportToDimension((EntityPlayer) living, 0, ((EntityPlayer) living).posX, 800, ((EntityPlayer) living).posZ);
         }
     }
