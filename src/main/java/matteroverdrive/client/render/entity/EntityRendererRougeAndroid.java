a7d469d81c3c3a4326c916631510982c1c96ef8e
Buuz135 <buuz135@hotmail.com>
2018-06-19 01:06:47 +0200

    Look at the colors! (#146)
    * Fixed teleport module teleporting the android inside blocks
    
    * Fixed keybindings not working properly
    
    * Commit brought to you thanks to corsair

--- src/main/java/matteroverdrive/client/render/entity/EntityRendererRougeAndroid.java
+++ src/main/java/matteroverdrive/client/render/entity/EntityRendererRougeAndroid.java
@@ -25,16 +25,26 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Simeon on 5/26/2015.
  */
 @SideOnly(Side.CLIENT)
 public class EntityRendererRougeAndroid<T extends EntityRougeAndroidMob> extends RenderBiped<T> {
-    public static final ResourceLocation texture = new ResourceLocation(Reference.PATH_ENTITIES + "android.png");
-    public static final ResourceLocation texture_hologram = new ResourceLocation(Reference.PATH_ENTITIES + "android_holo.png");
+
+    public static final ResourceLocation TEXTURE = new ResourceLocation(Reference.PATH_ENTITIES + "android.png");
+    public static final ResourceLocation TEXTURE_HOLOGRAM = new ResourceLocation(Reference.PATH_ENTITIES + "android_holo.png");
+    public static final ResourceLocation TEXTURE_COLORLESS = new ResourceLocation(Reference.PATH_ENTITIES + "android_colorless.png");
+    public static final List<String> NAMES = Arrays.asList("the_codedone","chairosto", "buuz135");
+
     private final boolean hologram;
 
     public EntityRendererRougeAndroid(RenderManager renderManager, ModelBiped modelBiped, float f, boolean hologram) {
@@ -48,10 +58,12 @@ public class EntityRendererRougeAndroid<T extends EntityRougeAndroidMob> extends
 
     @Override
     protected ResourceLocation getEntityTexture(T entity) {
-        if (hologram) {
-            return texture_hologram;
+        if (entity.hasCustomName() && NAMES.contains(TextFormatting.getTextWithoutFormattingCodes(entity.getCustomNameTag().toLowerCase()))){
+            return TEXTURE_COLORLESS;
+        } else if (hologram) {
+            return TEXTURE_HOLOGRAM;
         } else {
-            return texture;
+            return TEXTURE;
         }
     }
 
@@ -65,7 +77,14 @@ public class EntityRendererRougeAndroid<T extends EntityRougeAndroidMob> extends
         if (entity.getIsLegendary()) {
             GlStateManager.scale(1.5, 1.5, 1.5);
         }
+        if (entity.hasCustomName() && NAMES.contains(TextFormatting.getTextWithoutFormattingCodes(entity.getCustomNameTag().toLowerCase()))){
+            float speed = 360 * 0.2f;
+            int hsb = (int) (entity.world.getTotalWorldTime() % speed);
+            Color color = Color.getHSBColor(hsb / speed, 0.5f, 1f);
+            GlStateManager.color(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, 1.0f);
+        }
         super.preRenderCallback(entity, partialTicks);
     }
+
 
 }
